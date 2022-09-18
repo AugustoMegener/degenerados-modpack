f61ec09ad95464e0e628f2220851589ddb165b8e
Hydr0gen2 <vcgolem@gmail.com>
2022-09-18 19:21:47 -0300

    Create TFC.js

---
+++ modpack/kubejs/server_scripts/TFC.js
@@ -0,0 +1,20 @@
+// priority: 0
+
+settings.logAddedRecipes = true
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+console.info('Hello, World! (You will see this line every time server resources reload)')
+
+onEvent('recipes', event => {
+	[
+        'pickaxe_head', 
+        'propick_head', 
+        'axe_head',
+        'shovel_head'
+    ].forEach()
+})
+
+onEvent('item.tags', event => {
+})
