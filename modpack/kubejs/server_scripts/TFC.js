db692a2a7bd039fff5fba3d3eca5fbe130aff748
Hydr0gen2 <vcgolem@gmail.com>
2022-09-19 19:07:21 -0300

    Update TFC.js

--- modpack/kubejs/server_scripts/TFC.js
+++ modpack/kubejs/server_scripts/TFC.js
@@ -8,23 +8,7 @@ settings.logErroringRecipes = true
 console.info('Hello, World! (You will see this line every time server resources reload)')
 
 onEvent('recipes', event => {
-	[
-        'pickaxe_head', 
-        'propick_head', 
-        'axe_head',
-        'shovel_head',
-        'hoe_head',
-        'chisel_head',
-        'hammer_head',
-        'saw_blade',
-        'javelin_head',
-        'sword_blade',
-        'mace_head',
-        'knife_blade',
-        'scythe_blade'
-    ].forEach((i) => {
-        event.remove({"output": "tfc:ceramic/" + i + "_mold"})
-    })
+    event.remove({"output": /tfc:ceramic\/.*_mold/})
 })
 
 onEvent('item.tags', event => {
