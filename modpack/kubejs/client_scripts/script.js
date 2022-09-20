86eacd2b6941f57688597762d7e638ae18b9bc8f
Hydr0gen2 <vcgolem@gmail.com>
2022-09-20 19:23:21 -0300

    Update script.js

--- modpack/kubejs/client_scripts/script.js
+++ modpack/kubejs/client_scripts/script.js
@@ -5,4 +5,9 @@ console.info('Hello, World! (You will see this line every time client resources 
 onEvent('jei.hide.items', event => {
 	// Hide items in JEI here
 	// event.hide('minecraft:cobblestone')
+	event.hide(/tfc:ceramic\/.*head_mold/)
+	event.hide(/tfc:ceramic\/unfired.*head_mold/)
+	
+	event.hide(/tfc:ceramic\/.*blade_mold/)
+	event.hide(/tfc:ceramic\/unfired.*blade_mold/)
 })
