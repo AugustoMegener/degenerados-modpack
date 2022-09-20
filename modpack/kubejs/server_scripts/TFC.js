b87a57bde4c33d30eec39c64c6772c7dd2c58cdf
Hydr0gen2 <vcgolem@gmail.com>
2022-09-20 19:23:26 -0300

    Update TFC.js

--- modpack/kubejs/server_scripts/TFC.js
+++ modpack/kubejs/server_scripts/TFC.js
@@ -2,13 +2,44 @@
 
 settings.logAddedRecipes = true
 settings.logRemovedRecipes = true
-settings.logSkippedRecipes = false
+settings.logSkippedRecipes = true
 settings.logErroringRecipes = true
 
 console.info('Hello, World! (You will see this line every time server resources reload)')
 
 onEvent('recipes', event => {
-    event.remove({"type": "tfc:casting", "not": {"fluid":{"ingredient": "tfc:metal/copper"}}})
+    // Remove molds.
+    event.remove(
+        {output: /tfc:ceramic\/.*head_mold/}
+    )
+    event.remove(
+        {output: /tfc:ceramic\/unfired.*head_mold/}
+    )
+
+    event.remove(
+        {output: /tfc:ceramic\/.*blade_mold/}
+    )
+    event.remove(
+        {output: /tfc:ceramic\/unfired.*blade_mold/}
+    )
+    
+    // Change Copper anvil recipe.
+    event.remove({output: "tfc:metal/anvil/copper"})
+    event.custom(
+        {
+            "type": "tfc:welding",
+            "first_input": {
+                "item": "tfc:metal/double_ingot/copper"
+            },
+            "second_input": {
+                "item": "tfc:metal/double_ingot/copper"
+            },
+            "tier": 0,
+            "result": {
+                "item": "tfc:metal/anvil/copper"
+            }
+        }
+    )
 })
 
 onEvent('item.tags', event => {
